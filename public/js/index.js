9f5d4bca0cb14430634da2ec3e32d791ea486735
Danijel Veljkovic <vedaniel.eth@gmail.com>
2018-10-24 21:43:17 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,6 @@ var socket = io();
 socket.on('connect', function(){
   console.log('Connected to server.')
 
-  socket.emit('createMessage', {
-    from: 'Dan',
-    text: 'Hello everone!'
-  });
-
 });
 
 socket.on('disconnect', function(){
--- server/server.js
+++ server/server.js
@@ -16,14 +16,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log("New user connected");
 
-  socket.emit('newMessage', {
-    from: 'Mr. Server',
-    text: 'Hello client.',
-    createdAt: new Date()
-  });
 
   socket.on('createMessage', (msg)=>{
     console.log(msg);
+    io.emit('newMessage', {
+      from: msg.from,
+      text: msg.text,
+      createdAt: new Date().getTime()
+    })
   });
 
   socket.on('disconnect', ()=>{
